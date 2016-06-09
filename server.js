f464bd9df891e6e008dbb6fe1a385f5a1d60b766
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-09 23:27:14 +0530

    Added support for storing queries in database

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
+var ts = require('unix-timestamp');
 var Search = require('bing.search');
 var search = new Search('UPQQGrnCOJZiGxfJ1whHXFB8CNTS1IgS0QIYbnd1Bfk');
+var url = 'mongodb://localhost:27018/search_records';
 app.set('port',process.env.port || 5000);
 app.get('/api/imagesearch/:data',function(req,res){
   var query_string = req.params.data;
   var offset = req.query.offset || 10;
+  var query_record = {};
+  query_record.term=query_string;
+  query_record.when=new Date().toISOString();
+  console.log(query_record.when);
   search.images(query_string,
   {top: offset},
   function(err, results) {
-    console.log(results);
+    //console.log(results);
     var res_arr = [];
     var res_obj = {};
     for(var i in results)
@@ -19,15 +26,44 @@ app.get('/api/imagesearch/:data',function(req,res){
       res_obj.thumbnail=results[i].thumbnail.url;
       res_obj.context=results[i].sourceUrl;
       res_arr.push(res_obj);
-      
       res.obj = {};
     }
-    console.log(res_arr);
+    //console.log(res_arr);
+    if(res_arr.length>0)
+    {
+      MongoClient.connect(url,function(err,db){
+      if(err)
+      console.log(err);
+      else
+      {
+        var recent_search = db.collection('recent_search');
+        recent_search.insert(query_record,function(err,data){
+          if(err)
+          console.log(err);
+          else
+          console.log(JSON.stringify(data));
+          db.close();
+        });
+      }
+    });
+    }
     res.json(res_arr);
   }
 );
   console.log(req.params.data);
   console.log(req.query.offset);
+});
+app.get('/api/latest/imagesearch/',function(req,res){
+  
+  MongoClient.connect(url,function(err,db){
+    if(err)
+    console.log(err);
+    else
+    {
+      var recent_search = db.collection('recent_search');
+      
+    }
+  });
 });
 app.use('/',function(req,res){
   res.sendFile(__dirname+"/index.html");
